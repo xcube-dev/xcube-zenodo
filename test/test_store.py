2446418c2f2718d80a37c305ab8db8ea6f115327
konstntokas <konstantin.ntokas@brockmann-consult.de>
2025-03-17 13:26:12 +0100

    revert changes in unit tests

--- test/test_store.py
+++ test/test_store.py
@@ -222,9 +222,7 @@ class ZenodoDataStoreTest(unittest.TestCase):
     @pytest.mark.vcr()
     def test_preload_data_tar_gz(self):
         store = new_data_store(DATA_STORE_ID, root="6453099")
-        cache_store = store.preload_data(
-            "diaz2016_inputs_raw.zarr.tar.gz", blocking=True, silent=True
-        )
+        cache_store = store.preload_data(blocking=True, silent=True)
         cache_store.preload_handle.close()
 
         self.assertCountEqual(["diaz2016_inputs_raw.zarr"], cache_store.list_data_ids())
@@ -282,8 +280,7 @@ class ZenodoDataStoreTest(unittest.TestCase):
         self.assertIsInstance(ds, xr.Dataset)
         self.assertCountEqual([f"band_{i}" for i in range(1, 40)], list(ds.data_vars))
         self.assertEqual(ds["band_1"].shape, (971, 1149))
-        if cache_store.protocol == "file":
-            shutil.rmtree(cache_store.root)
+        shutil.rmtree(cache_store.root)
 
     @pytest.mark.vcr()
     def test_preload_data_download_fails(self):
